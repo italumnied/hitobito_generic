c0fd3fe1eba111c49a2fe84616b70809e673d144
Ubuntu <ubuntu@ov-6ff8f7.infomaniak.ch>
2026-01-25 01:20:13 +0000

    fix address openstreetmap link

--- app/helpers/people_generic_helper.rb
+++ app/helpers/people_generic_helper.rb
@@ -7,4 +7,14 @@ module PeopleGenericHelper
   def format_person_advertising(person)
     person.advertising_label
   end
+
+  def link_to_address(person)
+    # Only show address map link if user has show_details permission
+    return unless can?(:show_details, person)
+    
+    if [person.address, person.zip_code, person.town].all?(&:present?)
+      link_to(icon("map-marker-alt", class: "fa-2x"), person_address_url(person), target: "_blank",
+        rel: "noopener")
+    end
+  end
 end
